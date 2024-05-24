6adde1e71a3f3414064f3cc717642b7dbb5bbcdb
Álex Maciel <aweasley.gina@gmail.com>
2024-05-24 00:37:09 -0300

    feat: adicionando icone, foto no menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,4 +1,12 @@
-import { Drawer, useTheme } from '@mui/material'
+import { Drawer, 
+    useTheme, 
+    Avatar, 
+    Divider, 
+    List, 
+    ListItemButton, 
+    ListItemIcon, 
+    ListItemText, Icon } from '@mui/material'
+import InboxIcon from '@mui/icons-material/Inbox';
 import { Box } from '@mui/system'
 
 interface IMenuLateralProp {
@@ -10,7 +18,24 @@ export const MenuLateral: React.FC<IMenuLateralProp> = ({ children }) => {
     return (
         <>
             <Drawer variant='permanent'>
-                teste
+                <Box width={theme.spacing(28)} display="flex" flexDirection="column" height="100%">
+                    <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+                        <Avatar
+                            sx={{height: theme.spacing(12), width: theme.spacing(12)}} 
+                            src="/static/images/avatar/1.jpg" />
+                    </Box>
+                    <Divider />
+                    <Box flex={1}>
+                        <List component="nav">
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Icon>home</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Página inicial" />
+                            </ListItemButton>
+                        </List>
+                    </Box>
+                </Box>
             </Drawer>
             <Box height='100vh' marginLeft={theme.spacing(28)}>
                 {children}
